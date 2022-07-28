c57a0471c3aea9d8f111933f4d0f5de889d9c106
Eric Coleman <eric.coleman@frontierbpm.com>
2022-07-28 10:56:42 -0400

    added css, preformed tests on routes

--- tech-news-java-api/src/main/java/com/technews/repository/PostRepository.java
+++ tech-news-java-api/src/main/java/com/technews/repository/PostRepository.java
@@ -8,5 +8,5 @@ import com.technews.model.Post;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer>{
-    List<Post> findAllPostByUserId(Integer id) throws Exception;
+    List<Post> findAllPostsByUserId(Integer id) throws Exception;
 }
